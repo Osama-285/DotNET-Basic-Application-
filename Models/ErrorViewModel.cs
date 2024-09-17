24a1c5141a8cd62b14527539407776696d206d79
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-09-17 08:36:56 +0500

    dot net mvc application structure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotNET_Basic_Application_.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
